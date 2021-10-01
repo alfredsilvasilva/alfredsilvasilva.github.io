0b87edbdad9f1fcbbaeca7e2ef984deace83841f
alfredsilvasilva <alfredmetal@gmail.com>
2021-10-01 06:57:40 -0300

    add media query

--- Js/index.js
+++ Js/index.js
@@ -1,13 +1,10 @@
+// esta função mostra o menu mobile
+function clickEvent (){
+  var img=document.getElementById('nav');
+  if (img.style.display==="none"){
+      img.style.display = "block";
+  }else{
+      img.style.display="none"
+  }
+}
 
-const frutas =['banana', 'maçã', 'pera']
-const salgados =['coxinha', 'kibe ', ]
-const alimentos = frutas.concat(salgados)
-
-frutas.pop()
-frutas.push("goiaba")
-frutas.splice(0, 0, "melancia")
-frutas.unshift('laranja')
-frutas.slice(0, 3)
-console.log(frutas);
-console.log(alimentos)
-salgados.forEach((salgados,index) => console.log(index, salgados))
